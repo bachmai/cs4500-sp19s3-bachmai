066831371fe8731004a0e42656fb86186182a638
bachmai <bach.mai.viet@gmail.com>
2019-01-23 02:27:07 -0500

    added /api/user/{userId} retrieving the User object with said id

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.cs4500.models.User;
 
 /**
- * A REST Controller which:
- * 1) retrieves the list of all users.
- * 2) retrieving a particular user by id.
+ * A REST Controller which: 1) retrieves the list of all users. 2) retrieving a
+ * particular user by id.
  * 
  * Created on Jan 22, 2019
+ * 
  * @author bachm
  *
  */
@@ -21,17 +22,35 @@ import edu.neu.cs4500.models.User;
 public class UserService {
 	static List<User> users = new ArrayList<User>();
 	static User alice = new User(123, "alice", "password", "Alice", "Wonderland");
-	static User bach = new User(888, "bach", "1to8", "Bach", "Mai");
+	static User bach = new User(100, "bach", "1to8", "Bach", "Mai");
 	static {
 		users.add(alice);
 		users.add(bach);
 	}
+
 	/**
 	 * Retrieves the list of all users
+	 * 
 	 * @return list of all users
 	 */
 	@GetMapping("/api/users")
 	public List<User> getAllUsers() {
 		return users;
+	}
+
+	/**
+	 * Retrieves a particular user by id.
+	 * 
+	 * @param id the id of the user to be retrieved
+	 * @return the user with the given id
+	 */
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(@PathVariable("userId") Integer id) {
+		for (User user : users) {
+			if (user.getId() == id) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
